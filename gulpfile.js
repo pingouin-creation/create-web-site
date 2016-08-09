4e261e94c5c839ea60cc2234f744047e9bee4524
Maxime Trudel <max@pingouincreation.com>
2016-08-09 15:30:05 -0400

    Jade to pug & mofification in lint file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp');
-var args = require('yargs').argv;
 var del = require('del');
 var browsersync = require('browser-sync').create();
 var critical = require('critical').stream;
@@ -7,12 +6,8 @@ var checkPages = require('check-pages');
 var pngquant = require('imagemin-pngquant');
 var $ = require('gulp-load-plugins')({lazy: true, rename :{'gulp-minify-html-2': 'minifyHTML'}});
 
-
-
-
-
 gulp.task('css', function(){
-	return gulp
+return gulp
 		.src('./src/sass/styles.scss')
 		.pipe($.sass())
 		.pipe($.plumber())
@@ -25,35 +20,26 @@ gulp.task('css', function(){
 
 });
 
-gulp.task('vet', function() {
-	 return gulp
-	 .src('./src/js//*.js')
-	 .pipe($.if(args.verbose, $.print()))
-	 .pipe($.jshint())
-	 .pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
-	 .pipe($.jshint.reporter('fail'));
-});
-
-gulp.task('jade', function(){
+gulp.task('pug', function(){
 	return gulp
-	.src('./src/jade/*.jade')
-	.pipe($.jade({
+	.src('./src/pug/*.pug')
+	.pipe($.pug({
 		pretty:true
 	}))
 	.pipe(gulp.dest('./src/'));
 });
 
 gulp.task('retina', function(){
-	
+
 var retinizeOpts = {
   flags: {1: '_1x', 2: '_2x'},
   flagsOut: {1: ''},
-};	
+};
 	  return gulp
 	.src('./src/images/**/*.{png,jpg,jpeg}')
     .pipe($.retinize(retinizeOpts))
     .pipe(gulp.dest('./src/images/'));
- 
+
 });
 
 
@@ -67,7 +53,7 @@ gulp.task('image-optim', ['retina'], function(){
 });
 
 
-gulp.task('browser-sync',['css', 'vet', 'jade'] function(){
+gulp.task('browser-sync',['css', 'pug'], function(){
 	browsersync.init({
 		server: {
 			baseDir : './src/'
@@ -80,13 +66,13 @@ gulp.task('cleanbuild', function(){
 	return del('./build/**/*');
 });
 
-gulp.task('compiled',['css', 'vet', 'image-optim', 'jade'], function(){
+gulp.task('compiled',['css', 'image-optim', 'pug'], function(){
 	return gulp.src('./src/**/*.html')
 	.pipe ($.useref())
 	.pipe($.if('*.js', $.uglify()))
 	.pipe($.if('*.css', $.csso()))
 	.pipe(gulp.dest('./build/'));
-	
+
 });
 
 gulp.task('critical',['compiled'], function(){
@@ -104,7 +90,7 @@ gulp.task('critical',['compiled'], function(){
 
 
 gulp.task('minify_HTML', ['critical'], function(){
-	var opts = {spare:true}
+	var opts = {spare:true};
 
 	return gulp.src('./build/**/*.html')
     .pipe($.minifyHTML(opts))
@@ -115,7 +101,7 @@ gulp.task('build',['sitemap'] , function(){
 	browsersync.init({
 		server:'./build/'
 	});
-});	
+});
 
 gulp.task('testBuild', function(done){
 	var options = {
@@ -137,13 +123,13 @@ gulp.task('sitemap',['minify_HTML'], function(){
 	.pipe($.sitemap({
 		siteUrl: '[yoursite]',
 		changefreq: 'weekly',
-		
+
 	}))
-	.pipe(gulp.dest('./build'))
+	.pipe(gulp.dest('./build'));
 });
 
 gulp.task('watch', ['browser-sync'], function() {
 	gulp.watch('./src/js/*.js', ['vet']);
 	gulp.watch('./src/sass/**/*', ['css']);
-	gulp.watch('./src/jade/*.jade', ['jade']).on('change', browsersync.reload);
+	gulp.watch('./src/pug/*.pug', ['pug']).on('change', browsersync.reload);
 });

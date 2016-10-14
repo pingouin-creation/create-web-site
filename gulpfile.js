acf83c6337fa393e8b3a435abf3116acd90067e7
Maxime Trudel <max@pingouincreation.com>
2016-10-14 11:28:17 -0400

    modification to add responsive and ftp

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var browsersync = require('browser-sync').create();
 var critical = require('critical').stream;
 var checkPages = require('check-pages');
 var pngquant = require('imagemin-pngquant');
+var ftp = require( 'vinyl-ftp' );
+var gutil = require('gulp-util');
 var $ = require('gulp-load-plugins')({lazy: true, rename :{'gulp-minify-html-2': 'minifyHTML'}});
 
 gulp.task('css', function(){
@@ -29,18 +31,66 @@ gulp.task('pug', function(){
 	.pipe(gulp.dest('./src/'));
 });
 
+gulp.task('svg', function(){
+    return gulp.src('src/images/**/*.svg')
+    .pipe($.svgmin())
+    .pipe(gulp.dest('./build/images'));
+});
+
 gulp.task('retina', function(){
 
-var retinizeOpts = {
-  flags: {1: '_1x', 2: '_2x'},
-  flagsOut: {1: ''},
-};
-	  return gulp
-	.src('./src/images/**/*.{png,jpg,jpeg}')
-    .pipe($.retinize(retinizeOpts))
-    .pipe(gulp.dest('./src/images/'));
-
-});
+    return gulp.src('./src/images/*.{jpg,png}')
+        .pipe($.responsive({
+          // Resize all JPG images to three different sizes: 100, 200, and 400, 800 pixels
+          '*.jpg': [{
+            width: 100,
+            rename: { suffix: '' },
+          }, {
+            width: 200,
+            rename: { suffix: '_2x' },
+          }, {
+            width: 400,
+            rename: { suffix: '_3x' },
+          }, {
+            width: 800,
+            rename: { suffix: '_4x' },
+            },{
+          width: 1600,
+            rename: { suffix: '_full' },
+            },{
+            // Compress, strip metadata, and rename original image
+            rename: { suffix: '-original' },
+          }],
+          // Resize all PNG images to be retina ready
+          '*.png': [{
+            width: 100,
+            }, {
+            width: 200,
+            rename: { suffix: '_2x' },
+            }, {
+            width: 400,
+            rename: { suffix: '_3x' },
+            }, {
+            width: 800,
+            rename: { suffix: '_4x' },
+            }, {
+            width: 1600,
+            rename: { suffix: '_full' },
+        }],
+            }, {
+          // Global configuration for all images
+          // The output quality for JPEG, WebP and TIFF output formats
+          quality: 80,
+          // Use progressive (interlace) scan for JPEG and PNG output
+          progressive: true,
+          // Strip all metadata
+          withMetadata: false,
+          withoutEnlargement: true,
+          // Do not emit the error when image is enlarged.
+          errorOnEnlargement: false,
+        }))
+        .pipe(gulp.dest('./src/images/optim/'));
+    });
 
 
 gulp.task('image-optim', ['retina'], function(){
@@ -62,11 +112,12 @@ gulp.task('browser-sync',['css', 'pug'], function(){
 });
 
 
-gulp.task('cleanbuild', function(){
+gulp.task('cleanbuild', function(done){
 	return del('./build/**/*');
+    done();
 });
 
-gulp.task('compiled',['css', 'image-optim', 'pug'], function(){
+gulp.task('compiled',['cleanbuild','css', 'image-optim', 'pug'], function(){
 	return gulp.src('./src/**/*.html')
 	.pipe ($.useref())
 	.pipe($.if('*.js', $.uglify()))
@@ -83,7 +134,7 @@ gulp.task('critical',['compiled'], function(){
 		inline: true,
 		minify: true,
 		width: 1000,
-		height: 900
+		height: 1000
 		 }))
 	.pipe(gulp.dest('./build/'));
 });
@@ -132,4 +183,22 @@ gulp.task('watch', ['browser-sync'], function() {
 	gulp.watch('./src/js/*.js', ['vet']);
 	gulp.watch('./src/sass/**/*', ['css']);
 	gulp.watch('./src/pug/*.pug', ['pug']).on('change', browsersync.reload);
+});
+
+gulp.task('upload', ['build'], function(){
+    var conn = ftp.create( {
+       host:     'ftp.host.com',
+       user:     'username',
+       password: 'password',
+       parallel: 10,
+       log:      gutil.log
+   } );
+
+   var globs = [
+       './build/**/*',
+      ];
+
+      return gulp.src( globs, { buffer: false } )
+        .pipe( conn.dest( '/' ) );
+
 });
